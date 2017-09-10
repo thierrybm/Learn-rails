3fbb2683eadce14c3e0e1c9cc081878f7dc30703
Thierry Banza-Matulu <tbmatulu@gmail.com>
2017-09-10 17:13:25 -0400

    modified

--- controllers/visitors_controller.rb
+++ controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
   def new
+    Rails.logger.debug "DEBUG: Entering new method"
     @owner = Owner.new
+    Rails.logger.debug "DEBUG: owner name is " + @owner.name
   end
 end
--- models/owner.rb
+++ models/owner.rb
@@ -8,6 +8,7 @@ class Owner
   end
 
   def countdown
+    Rails.logger.debug "DEBUG: Entering Owner countdowm method"
     today = Date.today
     birthday = Date.new(today.year, birthdate.month,  birthdate.day)
     if birthday > today
